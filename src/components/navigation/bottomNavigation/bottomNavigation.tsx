e4681784327edb8502f68765c17e89e9554fc371
Victor <victor.trinh@outlook.com>
2020-11-30 17:58:25 -0500

    Fix bottom navigation

--- src/components/navigation/bottomNavigation/bottomNavigation.tsx
+++ src/components/navigation/bottomNavigation/bottomNavigation.tsx
@@ -2,11 +2,12 @@ import {
   BottomNavigation as MaterialBottomNavigation,
   BottomNavigationProps as MaterialBottomNavigationProps
 } from '@material-ui/core';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, ElementType } from 'react';
 
 export interface BottomNavigationProps
   extends Omit<MaterialBottomNavigationProps, 'onChange'> {
-  onChange: (event: ChangeEvent<{}>, newValue: any) => void;
+  onChange?: (event: ChangeEvent<{}>, newValue: any) => void;
+  component?: ElementType;
 }
 
 export const BottomNavigation: React.FC<BottomNavigationProps> = ({
