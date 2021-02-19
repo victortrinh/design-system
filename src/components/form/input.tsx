2f02269e3ec6eaf4a52edec1694cadca3bb03182
Victor <victor.trinh@outlook.com>
2021-02-19 17:14:11 -0500

    Fix input

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,10 +1,10 @@
 import { BaseTextFieldProps, TextField } from '@material-ui/core';
-
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 export interface InputProps extends BaseTextFieldProps {
   variant?: 'outlined' | 'filled' | 'standard';
   label?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Input: React.FC<InputProps> = ({ ...props }) => (
